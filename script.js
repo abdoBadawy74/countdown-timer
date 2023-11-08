b6a8de5ece04c4fa11c81a8310115d6ba93d39ba
abd074 <badawyabdulrahman099@gmail.com>
2023-11-08 18:12:28 +0200

    enhance style and make it dynamic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let countdown;
 let timerRunning = false;
 let endTime;
+let timeLeft;
 
 const startButton = document.getElementById('start');
 const stopButton = document.getElementById('stop');
@@ -20,13 +21,21 @@ function startTimer() {
       return;
     }
 
-    endTime = Date.now() + minutes * 60 * 1000;
+    if (!endTime) {
+      endTime = Date.now() + minutes * 60 * 1000;
+    } else {
+      endTime = Date.now() + timeLeft;
+    }
 
     timerRunning = true;
+    startButton.textContent = 'Continue';
     startButton.disabled = true;
 
     countdown = setInterval(updateCountdown, 1000);
     updateCountdown();
+  } else {
+    // If the timer is already running, this click is to continue it.
+    stopTimer();
   }
 }
 
@@ -34,24 +43,32 @@ function stopTimer() {
   if (timerRunning) {
     clearInterval(countdown);
     timerRunning = false;
+    startButton.textContent = 'Continue';
     startButton.disabled = false;
+    const now = Date.now();
+    timeLeft = endTime - now;
   }
 }
 
 function resetTimer() {
   clearInterval(countdown);
   timerRunning = false;
+  startButton.textContent = 'Start';
   startButton.disabled = false;
   minutesInput.value = '';
   countdownDisplay.textContent = '00:00';
+  endTime = undefined;
+  timeLeft = undefined;
 }
 
 function updateCountdown() {
-  const timeLeft = endTime - Date.now();
+  const now = Date.now();
+  timeLeft = endTime - now;
   if (timeLeft <= 0) {
     clearInterval(countdown);
     countdownDisplay.textContent = '00:00';
     timerRunning = false;
+    startButton.textContent = 'Start';
     startButton.disabled = false;
   } else {
     displayTime(timeLeft);
